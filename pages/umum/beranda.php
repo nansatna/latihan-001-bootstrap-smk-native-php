f14beeb3f48a244aef50720261cfb202b6bea795
Suharta <suharta64@guru.smk.belajar.id>
2025-09-16 14:14:23 +0700

    menghapus beranda di template dan update beranda pages umum

--- pages/umum/beranda.php
+++ pages/umum/beranda.php
@@ -10,11 +10,32 @@
 <!-- Tentang Sekolah -->
 <section id="profil" class="py-5">
   <div class="container text-center">
-    <h2 class="fw-bold mb-4">Tentang SMK Pasundan Subang</h2>
-    <p class="text-muted">
-      SMK Pasundan Subang berdiri dengan visi membangun sumber daya manusia unggul 
-      yang mampu bersaing di dunia kerja maupun perguruan tinggi.
-    </p>
+      <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <h2 class="fw-bold mb-4">Tentang SMK Pasundan Subang</h2>
+            <p class="text-muted">
+              SMK Pasundan Subang berdiri dengan visi membangun sumber daya manusia unggul 
+              yang mampu bersaing di dunia kerja maupun perguruan tinggi2.
+            </p>
+            <img src="..." class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
   </div>
 </section>
 
